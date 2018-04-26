71d67c64b5e998cf017fde8c8f6dca25dfc03e2d
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-04-27 00:48:50 +0900

    update multi_process_sample.c

--- multi_process/multi_process_sample.c
+++ multi_process/multi_process_sample.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-int main(void)
-{
-    printf("Hello World!\n");
+int main(void) {
+    int n = 1;
+    int status;
+    pid_t pid;
 
-    return 0;
+    sleep(10);
+
+    pid = fork();
+
+    if (pid < 0) {
+        perror("fork");
+
+        return 1;
+    } else if (pid == 0) {
+        printf("child  n[%p]=%d\n", &n, n);
+
+        sleep(10);
+
+        n = 2;
+        printf("child  n[%p]=%d\n", &n, n);
+
+        return 1;
+    } else {
+        printf("parent n[%p]=%d\n", &n, n);
+
+        wait(&status);
+
+        n = 3;
+        printf("parent n[%p]=%d\n", &n, n);
+
+        return 0;
+    }
 }
