1b51db86c970c234d3679b155c54e8a12f4e65db
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-05-02 18:43:12 +0900

    add multi_thread/multi_thread_challenge.c

---
+++ multi_thread/multi_thread_challenge.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <limits.h>
+
+void func() {
+    printf(".");
+    sleep(10);
+}
+
+int main(void) {
+    int i;
+    int ret;
+    pthread_t pthread;
+
+    for(i = 0; i < INT_MAX; i++) {
+        ret = pthread_create(&pthread, NULL, (void *)func, NULL);
+        if (ret != 0) {
+            printf("\nGenerated threads : %d\n", i);
+
+            return 1;
+        }
+    }
+
+    return 0;
+}
