85a34d9b2eb1fba8d71675dcb5ebf166ef5405d4
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-05-02 18:42:07 +0900

    add multi_process/multi_process_challenge.c

---
+++ multi_process/multi_process_challenge.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <limits.h>
+
+int main(void) {
+    int i;
+    int status;
+    int count;
+    pid_t pid;
+
+    for(i = 0; i < INT_MAX && (pid = fork()) > 0; i++);
+
+    if (pid == 0) {
+        printf(".");
+        sleep(1);
+
+        return 0;
+    } else {
+        count = i;
+        for(; i >= 0; i--) {
+            wait(&status);
+        }
+
+        printf("\nGenerated processes : %d\n", count);
+
+        return 1;
+    }
+}
