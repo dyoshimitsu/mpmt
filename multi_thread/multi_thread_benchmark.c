54a374cefa1ab3e855d974c2053038fca45a5ce8
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-05-02 16:36:18 +0900

    add multi_thread/multi_thread_benchmark.c

---
+++ multi_thread/multi_thread_benchmark.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+
+#define T_MAX 100
+
+void func() {
+    printf("sub\n");
+}
+
+int main(void) {
+    int i;
+    int ret;
+    pthread_t pthread[T_MAX];
+    clock_t start, end;
+
+    start = clock();
+
+    for(i = 0; i < T_MAX; i++) {
+        ret = pthread_create(&pthread[i], NULL, (void *)func, NULL);
+        if (ret != 0) {
+            perror("pthread_create");
+
+            return 1;
+        }
+    }
+
+    for(i = 0; i < T_MAX; i++) {
+        ret = pthread_join(pthread[i], NULL);
+        if (ret != 0) {
+            perror("pthread_join");
+
+            return 1;
+        }
+    }
+
+    end = clock();
+    printf("processing time : %lu[ms]\n", end - start );
+
+    return 0;
+}
